db5f38ca35cd48203a7a53d0e114c1870fefd1ca
alindobx <tonyromebx@gmail.com>
2020-01-04 02:31:44 -0500

    reversion 4 todo- App

--- js/to-do.js
+++ js/to-do.js
@@ -15,10 +15,31 @@ function addToDo(e) {
     div.innerHTML =
         `<input type="checkbox" class="form-check-input">
          <label class="form-check-label" for="exampleCheck1">${inputValue}</label>
-         <button type="button" class="btn btn-danger float-right btn-circle btn-sm">X</button>`;
+         <button type="button" id="test" class="btn btn-danger float-right btn-circle btn-sm delete">X</button>`;
     //append Child of receiving div
     toDoList.appendChild(div);
+    //clear the input field
+    document.getElementById('input-text').value = '';
 }
 
+//remove Button
+
+function removeToDo (e){
+    //Select the Button that needs to be removed
+    const remove = e.target.classList.contains('delete');
+    const removeDiv = e.target.parentElement;
+        remove
+            ? toDoList.removeChild(removeDiv)
+            : null;
+}
+const selectCheckbox = document.querySelector('.form-check');
+function checkBox (e) {
+
+}
+
+
+
 addBtn.addEventListener('click', addToDo);
+toDoList.addEventListener('click', removeToDo);
+selectCheckbox.addEventListener('')
 
