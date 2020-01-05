bcb11513de2ab5afc8ee4cc88ba3e8f6fa37c013
alindobx <tonyromebx@gmail.com>
2020-01-05 03:16:56 -0500

    revisions 1.0

--- js/to-do.js
+++ js/to-do.js
@@ -1,45 +1,44 @@
-//Select Submit Button
+
 const addBtn = document.getElementById('add-btn');
-//Select to do List id - that we need to append too
 const toDoList = document.getElementById('todo-list');
 
 //addBtn function
 function addToDo(e) {
     e.preventDefault();
-    //Get value of input field
     const inputValue = document.getElementById('input-text').value;
-    //Build the element to be put in the Text Node
-    const div = document.createElement('div');
-    div.className = "form-check";
-    //append textNode to value
-    div.innerHTML =
-        `<input type="checkbox" class="form-check-input">
+    if(inputValue === ''){
+        alert('You need to enter a todo')
+    }else {
+        const div = document.createElement('div');
+        div.className = "form-check";
+        //append textNode to value
+        div.innerHTML =
+            `<input type="checkbox" class="form-check-input" id="check">
          <label class="form-check-label" for="exampleCheck1">${inputValue}</label>
-         <button type="button" id="test" class="btn btn-danger float-right btn-circle btn-sm delete">X</button>`;
-    //append Child of receiving div
-    toDoList.appendChild(div);
-    //clear the input field
-    document.getElementById('input-text').value = '';
+         <i class="fas fa-times-circle float-right delete"></i>`;
+        //append Child of receiving div
+        toDoList.appendChild(div);
+        //clear the input field
+        document.getElementById('input-text').value = '';
+    }
 }
 
-//remove Button
-
 function removeToDo (e){
-    //Select the Button that needs to be removed
     const remove = e.target.classList.contains('delete');
     const removeDiv = e.target.parentElement;
         remove
             ? toDoList.removeChild(removeDiv)
             : null;
 }
-const selectCheckbox = document.querySelector('.form-check');
 function checkBox (e) {
-
+    const check = e.target.classList.contains('form-check-input');
+   e.target.nextElementSibling.classList.toggle('strike');
+        check
+        ? console.log('this is a check')
+        : null;
 }
-
-
 
 addBtn.addEventListener('click', addToDo);
 toDoList.addEventListener('click', removeToDo);
-selectCheckbox.addEventListener('')
+toDoList.addEventListener('click', checkBox);
 
