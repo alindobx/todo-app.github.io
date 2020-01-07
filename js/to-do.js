de841aec1a1b4d27879f879b1c3e48a6793182a6
alindobx <tonyromebx@gmail.com>
2020-01-07 00:23:33 -0500

    corrections 1.0

--- js/to-do.js
+++ js/to-do.js
@@ -1,44 +1,60 @@
 
 const addBtn = document.getElementById('add-btn');
 const toDoList = document.getElementById('todo-list');
+// collect Data
+const toDoArr = [];
+
+//grab local storage properties
+const data = JSON.parse(localStorage.getItem('input'));
+
+const divMaker = (text) =>{
+
+    const div = document.createElement('div');
+    div.className = "form-check";
+    //append textNode to value
+    div.innerHTML =
+        `<input type="checkbox" class="form-check-input" id="check">
+         <label class="form-check-label" for="exampleCheck1">${text}</label>
+         <i class="fas fa-times-circle float-right delete"></i>`;
+    //append Child of receiving div
+    toDoList.appendChild(div);
+};
 
 //addBtn function
 function addToDo(e) {
-    e.preventDefault();
     const inputValue = document.getElementById('input-text').value;
+    e.preventDefault();
+    console.log(toDoArr);
+    //sending input to array
+    toDoArr.push(inputValue);
     if(inputValue === ''){
         alert('You need to enter a todo')
     }else {
-        const div = document.createElement('div');
-        div.className = "form-check";
-        //append textNode to value
-        div.innerHTML =
-            `<input type="checkbox" class="form-check-input" id="check">
-         <label class="form-check-label" for="exampleCheck1">${inputValue}</label>
-         <i class="fas fa-times-circle float-right delete"></i>`;
-        //append Child of receiving div
-        toDoList.appendChild(div);
+        //send to local storage to Array
+        localStorage.setItem('input', JSON.stringify(toDoArr));
+        divMaker(inputValue);
         //clear the input field
         document.getElementById('input-text').value = '';
+
     }
 }
 
 function removeToDo (e){
     const remove = e.target.classList.contains('delete');
     const removeDiv = e.target.parentElement;
-        remove
-            ? toDoList.removeChild(removeDiv)
-            : null;
+    remove
+        ? toDoList.removeChild(removeDiv)
+        : null;
 }
 function checkBox (e) {
     const check = e.target.classList.contains('form-check-input');
-   e.target.nextElementSibling.classList.toggle('strike');
-        check
+    e.target.nextElementSibling.classList.toggle('strike');
+    check
         ? console.log('this is a check')
         : null;
 }
 
 addBtn.addEventListener('click', addToDo);
 toDoList.addEventListener('click', removeToDo);
-toDoList.addEventListener('click', checkBox);
+toDoList.addEventListener('change', checkBox);
 
