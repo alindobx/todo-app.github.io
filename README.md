0fb1e9beee30df98b8e613138609caeb9032897e
alindobx <tonyromebx@gmail.com>
2020-01-08 00:19:37 -0500

    final review

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 <h1>Vanilla Javascript To Do App</h1>
 <br>
-<p><em>Clock application written entirely in pure Javascript, HTML and CSS</em></p>
+<p><em>To Do application written entirely in pure Javascript, HTML and CSS</em></p>
 <br>
-<p><a href="https://alindobx.github.io/clock-app/index.html">Click Here</a> to Start Clock</p>
+<p><a href="https://alindobx.github.io/todo-app.github.io/?">Click Here</a> to Start Clock</p>
 <br>
 <p><img src="https://raw.githubusercontent.com/alindobx/todo-app.github.io/master/image/Screen%20Shot%202020-01-07%20at%2012.53.16%20AM.png" style="max-width:100;"></p>
 <h2>Summary</h2>
-<p>This simple clock Apps means alot because it is the first App I worked on with copying and pasting from another app</p>
-<p>I forced myself to go through the hurdles to fix the bugs and truly understand the code. I enjoyed the feeling I got when
-I worked out every issue</p>
+<p>This To Do Apps is a great way to keep track with your Day to Day line items of work</p>
 <h2>Author</h2>
 <ul>
   <li><strong>Anthony Lindo</strong> - <em>Full-Stack Software Developer - <a href="https://www.linkedin.com/in/anthony-lindo/">LinkedIn</a></li>
--- js/to-do.js
+++ js/to-do.js
@@ -1,65 +1,48 @@
+const data = JSON.parse(localStorage.getItem("input"));
 
-const addBtn = document.getElementById('add-btn');
-const toDoList = document.getElementById('todo-list');
-// collect Data
-const toDoArr = [];
-
-//grab local storage properties
-const data = JSON.parse(localStorage.getItem('input'));
-
-const divMaker = (text) =>{
-
-    const div = document.createElement('div');
-    div.className = "form-check";
-    //append textNode to value
-    div.innerHTML =
-        `<input type="checkbox" class="form-check-input" id="check">
+const toDoList = document.getElementById("todo-list");
+const divMaker = text => {
+  const div = document.createElement("div");
+  div.className = "form-check";
+  div.innerHTML = `<input type="checkbox" class="form-check-input" id="check">
          <label class="form-check-label" for="exampleCheck1">${text}</label>
          <i class="fas fa-times-circle float-right delete"></i>`;
-    //append Child of receiving div
-    toDoList.appendChild(div);
+  toDoList.appendChild(div);
 };
 
-//addBtn function
+const toDoArr = [];
+const addBtn = document.getElementById("add-btn");
 function addToDo(e) {
-    const inputValue = document.getElementById('input-text').value;
-    e.preventDefault();
-    console.log(toDoArr);
-    //sending input to array
-    toDoArr.push(inputValue);
-    if(inputValue === ''){
-        alert('You need to enter a todo')
-    }else {
-        //send to local storage to Array
-        localStorage.setItem('input', JSON.stringify(toDoArr));
-        divMaker(inputValue);
-        //clear the input field
-        document.getElementById('input-text').value = '';
-
-    }
+  const inputValue = document.getElementById("input-text").value;
+  e.preventDefault();
+  console.log(toDoArr);
+  toDoArr.push(inputValue);
+  if (inputValue === "") {
+    alert("You need to enter a todo");
+  } else {
+    localStorage.setItem("input", JSON.stringify(toDoArr));
+    divMaker(inputValue);
+    document.getElementById("input-text").value = "";
+  }
 }
 
-data.forEach(user =>{
-    divMaker(user);
-});
+localStorage.getItem("input") === null
+  ? console.log("true")
+  : data.forEach(user => {
+      divMaker(user);
+    });
 
-
-function removeToDo (e){
-    const remove = e.target.classList.contains('delete');
-    const removeDiv = e.target.parentElement;
-    remove
-        ? toDoList.removeChild(removeDiv)
-        : null;
+function removeToDo(e) {
+  const remove = e.target.classList.contains("delete");
+  const removeDiv = e.target.parentElement;
+  remove ? toDoList.removeChild(removeDiv) : null;
 }
-function checkBox (e) {
-    const check = e.target.classList.contains('form-check-input');
-    e.target.nextElementSibling.classList.toggle('strike');
-    check
-        ? console.log('this is a check')
-        : null;
+function checkBox(e) {
+  const check = e.target.classList.contains("form-check-input");
+  e.target.nextElementSibling.classList.toggle("strike");
+  check ? console.log("this is a check") : null;
 }
 
-addBtn.addEventListener('click', addToDo);
-toDoList.addEventListener('click', removeToDo);
-toDoList.addEventListener('change', checkBox);
-
+addBtn.addEventListener("click", addToDo);
+toDoList.addEventListener("click", removeToDo);
+toDoList.addEventListener("change", checkBox);
