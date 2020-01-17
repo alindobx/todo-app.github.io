d58d87e0e15fe517658c81601f389da249edc02e
alindobx <tonyromebx@gmail.com>
2020-01-16 20:31:31 -0500

    review notes

--- js/to-do.js
+++ js/to-do.js
@@ -1,48 +1,81 @@
 const data = JSON.parse(localStorage.getItem("input"));
-
 const toDoList = document.getElementById("todo-list");
-const divMaker = text => {
-  const div = document.createElement("div");
-  div.className = "form-check";
-  div.innerHTML = `<input type="checkbox" class="form-check-input" id="check">
-         <label class="form-check-label" for="exampleCheck1">${text}</label>
-         <i class="fas fa-times-circle float-right delete"></i>`;
-  toDoList.appendChild(div);
-};
-
-const toDoArr = [];
 const addBtn = document.getElementById("add-btn");
-function addToDo(e) {
-  const inputValue = document.getElementById("input-text").value;
-  e.preventDefault();
-  console.log(toDoArr);
-  toDoArr.push(inputValue);
-  if (inputValue === "") {
-    alert("You need to enter a todo");
-  } else {
-    localStorage.setItem("input", JSON.stringify(toDoArr));
-    divMaker(inputValue);
-    document.getElementById("input-text").value = "";
-  }
-}
+const toDoArr = [];
+
+addBtn.addEventListener("click", createTo);
 
 localStorage.getItem("input") === null
   ? console.log("true")
   : data.forEach(user => {
-      divMaker(user);
+      renderToDo(user);
     });
 
-function removeToDo(e) {
-  const remove = e.target.classList.contains("delete");
-  const removeDiv = e.target.parentElement;
-  remove ? toDoList.removeChild(removeDiv) : null;
-}
-function checkBox(e) {
-  const check = e.target.classList.contains("form-check-input");
-  e.target.nextElementSibling.classList.toggle("strike");
-  check ? console.log("this is a check") : null;
+function renderToDo(text) {
+  const div = document.createElement("div");
+  div.className = "form-check";
+
+  const uniqueIdentifier = Math.floor(Math.random() * new Date().getTime());
+
+  toDoList.appendChild(div);
+
+  div.appendChild(createInput(uniqueIdentifier));
+
+  div.appendChild(createCheckBox(text, uniqueIdentifier));
+
+  div.appendChild(createDeleteIcon(text));
 }
 
-addBtn.addEventListener("click", addToDo);
-toDoList.addEventListener("click", removeToDo);
-toDoList.addEventListener("change", checkBox);
+function createInput(id) {
+  const input = document.createElement("INPUT");
+  input.id = `checkbox-${id}`;
+  input.type = "checkbox";
+  input.className = "form-check-input";
+
+  input.onclick = function(e) {
+    e.target.nextElementSibling.classList.toggle("strike");
+  };
+  return input;
+}
+
+function createCheckBox(text, id) {
+  const label = document.createElement("LABEL");
+  label.className = "form-check-label";
+  label.setAttribute("for", `checkbox-${id}`);
+  label.textContent = text;
+  return label;
+}
+
+function createDeleteIcon(text) {
+  const deleteIcon = document.createElement("I");
+  deleteIcon.className = "fas fa-times-circle float-right delete";
+
+  deleteIcon.onclick = function(e) {
+    toDoList.removeChild(e.target.parentElement);
+    //pull in the local storage
+    const savedToDos = JSON.parse(localStorage.getItem("input"));
+    //delete the item from the list
+    const itemIndex = savedToDos.indexOf(text);
+    savedToDos.splice(itemIndex, 1);
+    //re-set the local storage
+    localStorage.setItem("input", JSON.stringify(savedToDos));
+  };
+
+  return deleteIcon;
+}
+
+function createTo(e) {
+  const inputValue = document.getElementById("input-text").value;
+  e.preventDefault();
+  toDoArr.push(inputValue);
+  if (inputValue === "") {
+    alert("You need to enter a todo");
+  } else {
+    let savedToDos = JSON.parse(localStorage.getItem("input"));
+    savedToDos = savedToDos || [];
+    savedToDos.push(inputValue);
+    localStorage.setItem('input', JSON.stringify(savedToDos));
+    renderToDo(inputValue);
+    document.getElementById("input-text").value = "";
+  }
+}
